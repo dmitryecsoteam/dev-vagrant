e62d1b0efe494aa5d3d8184e4855ad47ec75ea81
Dmitry Kiselev <dmitry.ecsoteam@gmail.com>
2018-10-18 13:56:25 +0300

    ansible_user to sudoers on command machine

--- vagrant-scripts/bootstrap-ansible.sh
+++ vagrant-scripts/bootstrap-ansible.sh
@@ -3,6 +3,7 @@ echo "------------ Adding ansible_user and SSH keys"
 adduser --gecos "" --disabled-password ansible_user
 echo "ansible_user:Qwerty12" | chpasswd
 adduser ansible_user sudo
+echo "ansible_user    ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 su -m "ansible_user" -c "ssh-keygen -b 2048 -t rsa -f /home/ansible_user/.ssh/id_rsa -N ''"
 printf "Host *\n  ForwardAgent yes\n" > /home/ansible_user/.ssh/config
 
